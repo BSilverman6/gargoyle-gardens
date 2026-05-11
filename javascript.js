39dcf6a27be481910bf6b0201ac9cd533353dd56
Bennett Silverman <152738859+BSilverman6@users.noreply.github.com>
2026-05-11 10:46:09 -0700

    markets updated

--- javascript.js
+++ javascript.js
@@ -6,6 +6,10 @@ const bennett = new Image();
 bennett.src ="images/bennett.JPG";
 bennett.alt = "pic o' Bennett";
 
+const market = new Image();
+market.src ="images/market.jpeg";
+market.alt = "pic o' market";
+
 
 set_text("home_b");
 
@@ -41,6 +45,14 @@ switch (id) {
         create_explore()
         break;
 
+    case "markets_b":
+        create_markets()
+        break;
+    
+    case "csa_b":
+        create_csa()
+        break;
+
     default:
         console.log("Nope")
         break;
@@ -53,9 +65,9 @@ function create_contact(){
     let phone = document.createElement("p");
     card.textContent = "Contact"
     email.textContent = "Email: Bennett@Gargoyle.Farm";
-    phone.textContent = "Phone: (___) ___ - ____";
+    //phone.textContent = "Phone: (___) ___ - ____";
     email.classList.add("center");
-    phone.classList.add("center");
+    //phone.classList.add("center");
     page_box.replaceChildren(card,email,phone)
 }
 
@@ -84,6 +96,42 @@ function create_about(){
 
 }
 
+function create_markets(){
+    const title = document.createElement("div");
+    const text = document.createElement("p");
+    const container = document.createElement("div");
+    container.classList.add("duo");
+
+    const subcontainer = document.createElement("div")
+    subcontainer.classList.add("trio");
+    const subcont1 = dalles_dates();
+    const subcont2 = ws_dates();
+    const subcont3 = vancouver_dates();
+    subcontainer.append(subcont1,subcont2,subcont3);
+    
+    title.textContent = "Markets";
+    text.textContent = 'Find us this summer in White Salmon, The Dalles, Vancouver, and Gorge Farmer Collective'
+    container.append(subcontainer, market);
+    page_box.replaceChildren(title,text,container);
+
+}
+function create_csa(){
+    const title = document.createElement("div");
+    const text = document.createElement("p");
+    const text2 = document.createElement("p");
+    const text3 = document.createElement("p");
+    const container = document.createElement("div");
+    container.classList.add("duo");
+    
+    title.textContent = "CSA";
+    text.textContent = 'Short About'
+    text2.textContent =  " The Details :) "
+    container.append(bennett,text2);
+    text3.textContent = "How to Join"
+    page_box.replaceChildren(title,text,container,text3);
+
+}
+
 function create_explore(){
     const title = document.createElement("div");
     title.textContent = "Magic Awaits!";
@@ -101,3 +149,71 @@ about_b.addEventListener("click", ()=> {
     about_text.textContent = "Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 Gargoyle Gardens. Gorge Grown Vegetables coming in 2026 "
     page_box.appendChild(about_text)
 }); */
+
+function dalles_dates(){
+    const myDiv = document.createElement("div");
+    const title = document.createElement("div"); title.textContent= "The Dalles, Saturdays"
+    const d1 = document.createElement("p"); d1.textContent= "June 13";
+    const d2 = document.createElement("p"); d2.textContent= "June 27";
+    const d3 = document.createElement("p"); d3.textContent= "July 11";
+    const d4 = document.createElement("p"); d4.textContent= "July 25";
+    const d5 = document.createElement("p"); d5.textContent= "August 8";
+    const d6 = document.createElement("p"); d6.textContent= "August 29";
+    const d7 = document.createElement("p"); d7.textContent= "September 12"
+    const d8 = document.createElement("p"); d8.textContent= "September 26";
+    const d9 = document.createElement("p"); d9.textContent= "October 10";
+    const d10 = document.createElement("p");d10.textContent= "October 24";
+
+    myDiv.classList.add("list")
+    myDiv.append(title,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10);
+    return myDiv;
+}
+
+function ws_dates(){
+    const myDiv = document.createElement("div");
+    const title = document.createElement("div"); title.textContent= "White Salmon, Every Tuesday"
+    const d1 = document.createElement("p"); d1.textContent= "June 2";
+    const d2 = document.createElement("p"); d2.textContent= "June 9";
+    const d3 = document.createElement("p"); d3.textContent= "June 16";
+    const d4 = document.createElement("p"); d4.textContent= "June 23";
+    const d5 = document.createElement("p"); d5.textContent= "June 30";
+    const d6 = document.createElement("p"); d6.textContent= "July 7";
+    const d7 = document.createElement("p"); d7.textContent= "July 14"
+    const d8 = document.createElement("p"); d8.textContent= "July 21";
+    const d9 = document.createElement("p"); d9.textContent= "July 28";
+    const d10 = document.createElement("p");d10.textContent= "August 4";
+    const d11 = document.createElement("p");d11.textContent= "August 11";
+    const d12 = document.createElement("p");d12.textContent= "August 18";
+    const d13 = document.createElement("p");d13.textContent= "August 25";
+    const d14 = document.createElement("p");d14.textContent= "September 1";
+    const d15 = document.createElement("p");d15.textContent= "August 8";
+    const d16 = document.createElement("p");d16.textContent= "August 15";
+    const d17 = document.createElement("p");d17.textContent= "August 22";
+    const d18 = document.createElement("p");d18.textContent= "August 29";
+
+
+
+    myDiv.classList.add("list")
+    myDiv.append(title,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16,d17,d18);
+    return myDiv;
+}
+
+function vancouver_dates(){
+    const myDiv = document.createElement("div");
+    const title = document.createElement("div"); title.textContent= "Downtown Vancouver, Sundays"
+    const d1 = document.createElement("p"); d1.textContent= "June 7";
+    const d2 = document.createElement("p"); d2.textContent= "June 21";
+    const d3 = document.createElement("p"); d3.textContent= "July 5";
+    const d4 = document.createElement("p"); d4.textContent= "July 19";
+    const d5 = document.createElement("p"); d5.textContent= "August 2";
+    const d6 = document.createElement("p"); d6.textContent= "August 23";
+    const d7 = document.createElement("p"); d7.textContent= "September 6"
+    const d8 = document.createElement("p"); d8.textContent= "September 20";
+    const d9 = document.createElement("p"); d9.textContent= "October 4";
+    const d10 = document.createElement("p");d10.textContent= "October 18";
+
+    myDiv.append(title,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10);
+    myDiv.classList.add("list")
+
+    return myDiv;
+}
