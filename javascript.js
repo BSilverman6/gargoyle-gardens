027f31f8373d04033273c938eabe9dd688df98b1
Bennett Silverman <152738859+BSilverman6@users.noreply.github.com>
2026-06-15 11:20:28 -0700

    added full dalles dates

--- javascript.js
+++ javascript.js
@@ -193,7 +193,7 @@ function dalles_dates(){
     const d16= document.createElement("p"); d16.textContent= "October 10";
 
     myDiv.classList.add("list")
-    myDiv.append(title,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10);
+    myDiv.append(title,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16);
     return myDiv;
 }
 
